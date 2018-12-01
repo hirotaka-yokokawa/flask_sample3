753e40e9adb5fb055337d5510c557e68584586c7
Kanuka <kanukamisaki@icloud.com>
2018-12-01 17:16:45 +0900

    for文追加
    Bob Tom Kenが表示される

--- omikuji.py
+++ omikuji.py
@@ -10,6 +10,12 @@ def fortune():
     idx = random.choice(["凶", "大吉", "吉"])
     return render_template("omikuji.html", idx=idx)
 
+@app.route("/members")
+def members():
+    members = ["Bob", "Tom","Ken"]
+
+
+    return render_template("members.html",members=members)
 
 if __name__ == "__main__":
     app.run(debug=True)
