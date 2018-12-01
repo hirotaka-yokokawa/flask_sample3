4f746a72e2545d0c95c7ce15bb0dc7e0c47d7e15
Hirotaka <yokokawa.491992@gmail.com>
2018-12-01 14:58:37 +0900

    ログインを実装

--- 01.py
+++ 01.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    print(request.method)
+    return "login page"
 
 
 if __name__ == "__main__":
